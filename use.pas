8f3dead1ff0e9ac0ed385dd640da167b0c74b0e1
WaldemarSky <vladimirkamensk92@gmail.com>
2021-05-16 22:38:45 +0300

    Revert "dkfkd"
    This reverts commit 36e375acae93f0fd8ea9335d028184ef387b7a34.

--- use.pas
+++ use.pas
@@ -1,2 +1,33 @@
-Error: Illegal parameter: -o lalala
-Error: /usr/bin/ppcx64 returned an error exitcode lololo
+program diamond;
+var
+    height, n, k, i: integer;
+begin
+    while (height mod 2 = 0) or (height <= 0) do begin
+        write('Please, enter your height of the diamond: ');
+        read(height);
+    end;
+    writeln;
+    n := height div 2;
+    for k := 0 to n do begin
+        for i := k to n - 1 do
+            write(' ');
+        write('*');
+        if k > 0 then begin
+            for i := 1 to 2*k - 1 do 
+                write(' ');
+            write('*')
+        end;
+        writeln
+    end;
+    for k := n - 1 downto 0 do begin
+        for i := k to n - 1 do
+            write(' ');
+        write('*');
+        if k > 0 then begin
+            for i := 1 to 2*k - 1 do
+                write(' ');
+            write('*')
+        end;
+        writeln
+    end
+end.
