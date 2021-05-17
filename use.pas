97e577fae4196338f2a03e17552d1de6bee7d77a
WaldemarSky <vladimirkamensk92@gmail.com>
2021-05-17 12:48:48 +0300

    added procedures

--- use.pas
+++ use.pas
@@ -1,6 +1,26 @@
 program diamond;
+
+procedure PrintSpaces (count: integer);
+var i:integer;
+begin
+    for i:= 1 to count do
+        write(' ')
+end;
+
+procedure PrintLineOfDiamond (k, n: integer);
+begin
+    PrintSpaces(n - k);
+    write('*');
+    if k>0 then begin
+        PrintSpaces(2*k - 1);
+        write('*')
+    end;
+    writeln
+end;
+
 var
     height, n, k, i: integer;
+
 begin
     while (height mod 2 = 0) or (height <= 0) do begin
         write('Please, enter your height of the diamond: ');
@@ -8,7 +28,15 @@ begin
     end;
     writeln;
     n := height div 2;
-    for k := 0 to n do begin
+    for k := 0 to n do
+       PrintLineOfDiamond (k, n);
+    for k := n - 1 downto 0 do
+       PrintLineOfDiamond (k, n)
+end.
+        
+
+
+{    for k := 0 to n do begin
         for i := k to n - 1 do
             write(' ');
         write('*');
@@ -30,4 +58,4 @@ begin
         end;
         writeln
     end
-end.
+end.}
